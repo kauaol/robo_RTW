e25c1a1f6fd9020c6484530e32aeb9208b18dfcb
kauadevv <kauah5670@gmail.com>
2025-07-02 07:03:54 -0300

    Adicionando o Headless

--- K_bot.py
+++ K_bot.py
@@ -282,7 +282,7 @@ if __name__ == "__main__":
     chrome_options = Options()
     chrome_options.add_argument("--start-maximized")
     chrome_options.add_argument("--log-level=3")
-    # chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
